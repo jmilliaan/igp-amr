4ca57689fc7540c47e550797c0692fa11a06c32b
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-11 16:24:18 +0700

    pi camera program

--- lidar3.py
+++ lidar3.py
@@ -4,5 +4,6 @@ lidar = PyLidar3.YdLidarX4(port="COM8")
 lidar.Connect()
 
 for i in range(1000):
-    print(lidar.StartScanning())
+    data = lidar.StartScanning()
+    print(data)
 lidar.Disconnect()
