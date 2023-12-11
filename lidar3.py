0b7b34af0b858a5f2ce0dd3459b91f4193909fc3
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-11 15:22:35 +0700

    add lidar3.py

--- lidar3.py
+++ lidar3.py
@@ -1,8 +1,4 @@
-import time
 import PyLidar3
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
 lidar = PyLidar3.YdLidarX4(port="COM8")
 lidar.Connect()
