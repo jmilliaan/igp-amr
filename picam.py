f4fed74ec7fe08c127447f044c81b6fb14c3d595
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-12 10:30:28 +0700

    picam

--- picam.py
+++ picam.py
@@ -11,17 +11,12 @@ def main():
         cv2.namedWindow("Raspberry Pi Camera", cv2.WINDOW_NORMAL)
 
         try:
-            # Capture frames from the camera
             with picamera.array.PiRGBArray(camera) as stream:
                 while True:
-                    # Capture a frame from the camera
                     camera.capture(stream, format="bgr", use_video_port=True)
                     image = stream.array
-
-                    # Display the frame in the OpenCV window
                     cv2.imshow("Raspberry Pi Camera", image)
 
-                    # Check for 'q' key to exit the loop
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
