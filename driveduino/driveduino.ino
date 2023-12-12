9ee494c2fb6abbf42a8b15691d89283230e7b8c5
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-12 16:10:06 +0700

    add visualization program

--- driveduino/driveduino.ino
+++ driveduino/driveduino.ino
@@ -1,9 +1,15 @@
-#include "MotorControl.h"
-#include "SerialComm.h"
-void setup(){
-  initializeComm(9600);
-};
+// #include "MotorControl.h"
+// #include "SerialComm.h"
+void setup() {
+  Serial.begin(9600);  // Set the baud rate to match the Raspberry Pi
+}
 
-void loop(){
-  recvData();
-};
+void loop() {
+  if (Serial.available() > 0) {
+    int receivedData = Serial.parseInt();  // Read the integer from the serial port
+
+    // Print the received data to the Serial Monitor
+    Serial.print("Received Data: ");
+    Serial.println(receivedData);
+  }
+}
