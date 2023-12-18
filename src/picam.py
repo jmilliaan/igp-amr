6d32dfbef372ddc17ba66a510780daa8b5e20bf1
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-18 16:04:40 +0700

    add camera test code

--- src/picam.py
+++ src/picam.py
@@ -1,63 +1,48 @@
-import picamera
+import cv2
+import numpy as np
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import time
 
-class RaspberryPiCamera:
-    def __init__(self, resolution=(1280, 720), framerate=30):
-        """
-        Initialize the Raspberry Pi camera with specified resolution and framerate.
+class PiCamOpenCV:
+    def __init__(self, resolution=(640, 480), framerate=32):
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.raw_capture = PiRGBArray(self.camera, size=resolution)
 
-        Args:
-            resolution (tuple): Resolution of the captured image/video (width, height).
-            framerate (int): Frames per second for video capture.
-        """
-        self.camera = picamera.PiCamera(resolution=resolution, framerate=framerate)
+    def show_image(self, save_to_png=False):
+        self.camera.capture(self.raw_capture, format="bgr")
+        image = self.raw_capture.array
 
-    def capture_continuous(self, callback_function):
-        """
-        Capture frames continuously and call the provided callback function for each frame.
+        if save_to_png:
+            cv2.imwrite("captured_image.png", image)
 
-        Args:
-            callback_function: Function to be called with each captured frame as an argument.
-        """
-        # Start capturing frames
-        for frame in self.camera.capture_continuous(format="bgr", use_video_port=True):
-            # Pass the frame to the callback function
-            callback_function(frame)
-
-    def adjust_camera_settings(self, **kwargs):
-        """
-        Adjust camera settings like ISO, contrast, saturation, etc.
-
-        Args:
-            **kwargs: Key-value pairs of camera settings and their values to adjust.
-        """
-        for key, value in kwargs.items():
-            self.camera.brightness = value if key == "brightness" else getattr(self.camera, key, value)
-
-    def preview_stream(self, window_name="Raspberry Pi Camera Stream"):
-        
-        cv2.namedWindow(window_name)
-        while True:
-            frame = self.camera.capture_continuous(use_video_port=True)
-            frame = next(frame.frames)
-            cv2.imshow(window_name, frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+        cv2.imshow("Captured Image", image)
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def close_camera(self):
-        """
-        Release resources and close the camera.
-        """
+    def show_live_stream(self):
+        for frame in self.camera.capture_continuous(self.raw_capture, format="bgr", use_video_port=True):
+            image = frame.array
+
+            cv2.imshow("Live Stream", image)
+            key = cv2.waitKey(1) & 0xFF
+
+            self.raw_capture.truncate(0)
+
+            if key == ord("q"):
+                break
+
+    def __del__(self):
         self.camera.close()
 
-# Example usage
-def process_frame(frame):
-    # Implement your frame processing logic here
-    # For example, perform object detection, track motion, etc.
-    # (This example simply displays the frame size)
-    print(f"Frame size: {frame.array.shape[:2]}")
+if __name__ == "__main__":
+    # Example usage
+    picam_opencv = PiCamOpenCV()
 
-camera = RaspberryPiCamera()
+    # Show a single image
+    picam_opencv.show_image(save_to_png=True)
 
-# Start continuous capture and use your custom processing function
-camera.preview_stream()
+    # Show live video stream
+    picam_opencv.show_live_stream()
