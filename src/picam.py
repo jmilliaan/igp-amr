eacbac9790512f0e8e2a70efeeb9f1a5d9ee8719
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-18 15:44:39 +0700

    add small trial code for picam

--- src/picam.py
+++ src/picam.py
@@ -3,47 +3,79 @@ import cv2
 
 class RaspberryPiCamera:
     def __init__(self, resolution=(1280, 720), framerate=30):
+        """
+        Initialize the Raspberry Pi camera with specified resolution and framerate.
+
+        Args:
+            resolution (tuple): Resolution of the captured image/video (width, height).
+            framerate (int): Frames per second for video capture.
+        """
         self.camera = picamera.PiCamera(resolution=resolution, framerate=framerate)
 
     def capture_image(self, filename="image.jpg"):
+        """
+        Capture an image and save it to the specified filename.
+
+        Args:
+            filename (str): Path and filename to save the captured image.
+        """
         self.camera.capture(filename)
 
-    def start_recording(self, filename="video.h264"):
-        self.camera.start_recording(filename)
+    def capture_single_frame(self, filename="frame.jpg"):
+        """
+        Capture a single frame from the camera and save it to the specified filename.
 
-    def stop_recording(self):
-        self.camera.stop_recording()
+        Args:
+            filename (str): Path and filename to save the captured frame.
+        """
+        # Warm up the camera
+        print("Warming up camera...")
+        self.camera.start_preview()
+        sleep(2)
+
+        # Capture and save the frame
+        print("Capturing frame...")
+        self.camera.capture(filename)
+
+        # Stop preview and close camera
+        print("Cleaning up...")
+        self.camera.stop_preview()
+        self.camera.close()
 
     def adjust_camera_settings(self, **kwargs):
+        """
+        Adjust camera settings like ISO, contrast, saturation, etc.
+
+        Args:
+            **kwargs: Key-value pairs of camera settings and their values to adjust.
+        """
         for key, value in kwargs.items():
             self.camera.brightness = value if key == "brightness" else getattr(self.camera, key, value)
 
     def preview_stream(self, window_name="Raspberry Pi Camera Stream"):
+        """
+        Preview the camera stream in a named window.
+
+        Args:
+            window_name (str): Name of the window displaying the camera stream.
+        """
         cv2.namedWindow(window_name)
         while True:
-            frame = self.camera.capture_continuous(format="bgr", use_video_port=True).next()
+            frame = self.camera.capture_continuous(format="bgr", use_video_port=True)
+            frame = next(frame.frames)
             cv2.imshow(window_name, frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         cv2.destroyAllWindows()
 
     def close_camera(self):
+        """
+        Release resources and close the camera.
+        """
         self.camera.close()
 
-# Check camera functionality if script is directly run
-if __name__ == "__main__":
-    # Create camera object
-    camera = RaspberryPiCamera()
+# Example usage
+camera = RaspberryPiCamera()
+camera.capture_single_frame("captured_frame.jpg")
 
-    # Preview camera stream for 5 seconds
-    print("Previewing camera stream for 5 seconds...")
-    camera.preview_stream()
-    cv2.waitKey(5000)
-
-    # Capture an image for confirmation
-    print("Capturing test image...")
-    camera.capture_image("camera_test.jpg")
-
-    # Print success message and close camera
-    print("Camera functionality test successful!")
-    camera.close()
+print("Frame captured successfully!")
