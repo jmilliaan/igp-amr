c7acf9a1521d11a9485cf32826e4d37a3dbc1199
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-19 08:47:18 +0700

    adjust sercomm

--- src/simple_serial.py
+++ src/simple_serial.py
@@ -5,13 +5,15 @@ arduino_port = "/dev/ttyUSB0"
 lidar_port = "/dev/ttyUSB3"
 
 lidar = PyLidar3.YdLidarX4(
-    port=lidar_port
-    )
+    port=lidar_port)
 
 lidar.Connect()
-lidar.GetDeviceInfo()
-lidar.GetHealthStatus()
+print("Device Info: ", end="")
+print(lidar.GetDeviceInfo())
+print("Health Info: ", end="")
+print(lidar.GetHealthStatus())
 lidar._Start_motor()
+
 def lidar_data():
     try:
         scanning_generator = lidar.StartScanning()
