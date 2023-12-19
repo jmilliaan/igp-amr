adbf1b96f10208635617210ab6069e028525ec81
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-19 12:31:51 +0700

    aadd comm verification

--- src/dual_serial.py
+++ src/dual_serial.py
@@ -19,10 +19,14 @@ def check_port_connection():
     if n_ports >= n_connections:
         for port in ports:
             print(port)
+            try_lidar = PyLidar3.YdLidarX4(port=port)
+            try_lidar.Connect()
+            time.sleep(0.5)
+            try_lidar.Disconnect()
+        return (True, connections)
     else:
-        print(f"At least {str(n_ports - n_connections)} device(s) is not connected!")
-        return
-    return connections
+        print(f"At least {str(n_connections - n_ports)} device(s) is not connected!")
+        return (False, connections)
 
 def lidar_data(lidar_obj):
     try:
@@ -39,11 +43,16 @@ def send_data(comm_obj):
     comm_obj.send_repeating_data(2)
 
 if __name__ == "__main__":
-    if check_port_connection():
-        lidar = PyLidar3.YdLidarX4(port=lidar_port)
-        drive_comm = SerialCommunication(
-            port=arduino_port, 
-            baud_rate=9600)
+    connection_check = check_port_connection()
+    connection_status = connection_check[0]
+    lidar_port = connection_check[1]["lidar"]
+    arduino_port = connection_check[1]["arduino"]
+    if connection_status:
+        print(connection_check)
+        # lidar = PyLidar3.YdLidarX4(port=lidar_port)
+        # drive_comm = SerialCommunication(
+        #     port=arduino_port, 
+        #     baud_rate=9600)
     else: 
         print("Bad port connection!")
     # lidar.Connect()
