fd5b861c40879fb48a3374d074d9bca3e856f0b6
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-19 13:02:16 +0700

    aadd comm verification

--- src/dual_serial.py
+++ src/dual_serial.py
@@ -16,10 +16,10 @@ def check_port_connection():
     
         for i in range(n_ports):
             other_idx = (i + 1) % n_ports
+            print(ports[i])
             try_lidar = PyLidar3.YdLidarX4(port=ports[i])
             try_lidar.Connect()
             device_info = try_lidar.GetDeviceInfo()
-    
             if device_info["model_number"] != '0':
                 print(f"port {ports[i]} is lidar")
                 connections["lidar"] = ports[i]
