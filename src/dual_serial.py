9f4db00eeee481697a815047638075cef3fb90a3
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-19 13:24:48 +0700

    aadd comm verification

--- src/dual_serial.py
+++ src/dual_serial.py
@@ -5,33 +5,35 @@ from serial_communication import SerialCommunication
 import time
 import glob
 
-LIDAR_IDX = 0
-ARDUINO_IDX = 1
-
-def connect_lidar(port):
-    lidar = PyLidar3.YdLidarX4(port=port)
-    lidar.Connect()
-    device_info = lidar.GetDeviceInfo()
-    return lidar if device_info["model_number"] != '0' else None
-
-def detect_connected_devices():
-    connections = {"lidar": "", "arduino": ""}
+def check_port_connection():
+    connections = {"lidar":"", "arduino":""}
+    n_connections = len(connections.keys())
+    
     ports = glob.glob("/dev/ttyUSB*")
+    n_ports = len(ports)
+    end_connection = False
+    if n_ports >= n_connections:    
+        for i in range(n_ports):
+            other_idx = not i
+            try_lidar = PyLidar3.YdLidarX4(port=ports[i])
+            try_lidar.Connect()
+            device_info = try_lidar.GetDeviceInfo()
+            
+            if device_info["model_number"] != '0':
+                connections["lidar"] = ports[i]
+                connections["arduino"] = ports[other_idx]
+                end_connection = True
+                time.sleep(0.2)
+                break
+            try_lidar.Disconnect()
 
-    if len(ports) >= len(connections):
-        for i, port in enumerate(ports):
-            other_idx = ARDUINO_IDX if i == LIDAR_IDX else LIDAR_IDX
+        if end_connection:
+            try_lidar.Disconnect()
 
-            with connect_lidar(port) as lidar:
-                if lidar:
-                    connections["lidar"] = port
-                    connections["arduino"] = ports[other_idx]
-                    break
-
-        return True, connections
+        return (True, connections)
     else:
-        print(f"At least {len(connections) - len(ports)} device(s) is not connected!")
-        return False, connections
+        print(f"At least {str(n_connections - n_ports)} device(s) is not connected!")
+        return (False, connections)
 
 def lidar_data(lidar_obj):
     try:
@@ -48,7 +50,7 @@ def send_data(comm_obj):
     comm_obj.send_repeating_data(2)
 
 if __name__ == "__main__":
-    connection_check = detect_connected_devices()
+    connection_check = check_port_connection()
     connection_status = connection_check[0]
     lidar_port = connection_check[1]["lidar"]
     arduino_port = connection_check[1]["arduino"]
