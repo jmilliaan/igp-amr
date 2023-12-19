de327bedfa5cebf86f1d6cf4faa4000a57e15e5f
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-19 08:20:52 +0700

    add threading for parallel sercomm

---
+++ src/simple_serial.py
@@ -0,0 +1,22 @@
+import PyLidar3
+from serial_communication import SerialCommunication
+
+arduino_port = "/dev/ttyUSB0"
+lidar_port = "/dev/ttyUSB1"
+
+lidar = PyLidar3.YdLidarX4(
+    port=lidar_port
+    )
+
+lidar.Connect()
+
+def lidar_data():
+    try:
+        scanning_generator = lidar.StartScanning()
+        while True:
+            scan_result = next(scanning_generator)
+            print("Lidar Scan Result:", scan_result)
+    except Exception as e:
+        print(f"Lidar scanning error: {e}")
+    finally:
+        lidar.Disconnect()
