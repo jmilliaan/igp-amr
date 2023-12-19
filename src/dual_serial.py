a93637b6a17ab06cadec165ebdbfa251030f80d0
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2023-12-19 11:21:02 +0700

    comm port list

--- src/dual_serial.py
+++ src/dual_serial.py
@@ -3,47 +3,50 @@ import multiprocessing
 import PyLidar3
 from serial_communication import SerialCommunication
 import time
+import glob
 
+usb_ports = glob.glob("/dev/ttyUSB*")
+print(usb_ports)
 arduino_port = "/dev/ttyUSB1"
 lidar_port = "/dev/ttyUSB0"
 
 
-lidar = PyLidar3.YdLidarX4(
-    port=lidar_port
-    )
-drive_comm = SerialCommunication(
-    port=arduino_port, 
-    baud_rate=9600
-    )
-
-lidar.Connect()
-
-def lidar_data():
+def lidar_data(lidar_obj):
     try:
-        scanning_generator = lidar.StartScanning()
+        scanning_generator = lidar_obj.StartScanning()
         while True:
             scan_result = next(scanning_generator)
             print("Lidar Scan Result:", scan_result)
     except Exception as e:
         print(f"Lidar scanning error: {e}")
     finally:
-        lidar.Disconnect()
+        lidar_obj.Disconnect()
 
-def send_data():
-    drive_comm.send_repeating_data(2)
+def send_data(comm_obj):
+    comm_obj.send_repeating_data(2)
 
 if __name__ == "__main__":
-    lidar_thread = threading.Thread(target=lidar_data)
-    sercomm_process = multiprocessing.Process(target=send_data)
-    try:
-        lidar_thread.start()
-        sercomm_process.start()
-        lidar_thread.join()
-        sercomm_process.join()
-    except KeyboardInterrupt:
-        for i in range(4):
-            drive_comm.write(1)
-            time.sleep(0.1)
-            
-        lidar_thread.join()
-        sercomm_process.join()
+    lidar = PyLidar3.YdLidarX4(
+    port=lidar_port
+    )
+    drive_comm = SerialCommunication(
+        port=arduino_port, 
+        baud_rate=9600
+        )
+
+    # lidar.Connect()
+
+    # lidar_thread = threading.Thread(target=lidar_data, args=(lidar,))
+    # sercomm_process = multiprocessing.Process(target=send_data, args=(drive_comm,))
+    # try:
+    #     lidar_thread.start()
+    #     sercomm_process.start()
+    #     lidar_thread.join()
+    #     sercomm_process.join()
+    # except KeyboardInterrupt:
+    #     for i in range(4):
+    #         drive_comm.write(1)
+    #         time.sleep(0.1)
+
+    #     lidar_thread.join()
+    #     sercomm_process.join()
