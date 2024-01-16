529f9d06aa25015984355c94631600527c849e28
Denata <denata.rizky@gmail.com>
2024-01-16 12:51:29 +0700

    Add client UI using vuejs

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <BaseTemplate />
+</template>
+
+<script>
+import BaseTemplate from './components/Base.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BaseTemplate
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Automatic.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class='flex-container-button'>
+        <button class='point-button' @click="sendCommand('point A')">Point A</button>
+        <button class='point-button' @click="sendCommand('point B')">Point B</button>
+        <button class='point-button' @click="sendCommand('point C')">Point C</button>
+    </div>
+</template>
+
+<style>
+    .flex-container-button {
+        display: flexbox;
+        justify-content: center;
+        place-items: center;
+        flex-direction: column;
+        padding-top:20px;
+    }
+    .point-button {
+        margin: 10px;
+        padding: 10px;
+        border-radius: 4px;
+        border: 1px solid #ddd;
+        background-color: #fff;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+    
+    }
+</style>
+<script>
+// import axios from 'axios';
+
+export default {
+    name: 'AutomaticControl',
+    props: {
+        msg: String,
+        ws: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        sendCommand(point) {
+            console.log(process.env.VUE_APP_WS_URL, point);
+            // axios.post(`${process.env.VUE_APP_API_URL}/point`, {
+            //     point: point
+            // }).then(response => {
+            //     console.log(response.data)
+            // }).catch(error => {
+            //     console.log(error)
+            // })
+        }
+    }
+}
+</script>
