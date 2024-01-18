bf0b4e74785d354771b7791a8aa25c1d38c285cc
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2024-01-18 11:26:44 +0700

    Update dual_serial.py

--- src/dual_serial.py
+++ src/dual_serial.py
@@ -10,14 +10,12 @@ This program integrates:
 Outputs the command to send to the arduino (movement commend)
 '''
 
-
 # import threading
 # import multiprocessing
 # import PyLidar3
 from serial_communication import SerialCommunication
 import time
 import glob
-from lidarprocessing import LIDAR
 import adafruit_rplidar
 # from picam import PiCam
 # from mapcalc import Map
@@ -36,39 +34,7 @@ web_to_serial_command = {
     9: 1,
 }
 
-# def check_port_connections():
-#     connections = {"lidar":"", "arduino":""}
-#     ports = glob.glob("/dev/ttyUSB*")
-#     n_ports = len(ports)
-#     print(f"SERIAL PORTS: {ports}")
-#     if n_ports == 1:
-#         connections["arduino"] = ports[0]
-#         connections["lidar"] = "None"
-#         return connections["arduino"], connections["lidar"]
-#     else:
-#         expected_lidar_port = ports[1]
-#         expected_arduino_port = ports[0]
-#         test_conn = adafruit_rplidar.RPLidar(
-#                 None, 
-#                 expected_lidar_port, 
-#                 timeout=3)
-#         try:  
-#             health = test_conn.health
-#             if isinstance(health, tuple):
-#                 connections["lidar"] = expected_lidar_port
-#                 connections["arduino"] = expected_arduino_port
-#             del health
-#             del test_conn
-#             return connections["arduino"], connections["lidar"]
-#         except adafruit_rplidar.RPLidarException:
-#             health = 0
-#             connections["lidar"] = expected_arduino_port
-#             connections["arduino"] = expected_lidar_port
-#             del health
-#             del test_conn
-#             return connections["arduino"], connections["lidar"]
-
-def check_port_connections():
+def setup_serial_port():
     connections = {"lidar": None, "arduino": None}
     ports = glob.glob("/dev/ttyUSB*")
     n_ports = len(ports)
@@ -92,112 +58,6 @@ def check_port_connections():
     return connections["arduino"], connections["lidar"]
 
 
-# def check_port_connection():
-#     connections = {"lidar":"", "arduino":""}
-#     n_connections = len(connections.keys())
-    
-#     ports = glob.glob("/dev/ttyUSB*")
-#     n_ports = len(ports)
-#     end_connection = False
-#     if n_ports >= n_connections:    
-#         for i in range(n_ports):
-#             other_idx = not i
-#             try_lidar = PyLidar3.YdLidarX4(port=ports[i])
-#             try_lidar.Connect()
-#             device_info = try_lidar.GetDeviceInfo()
-            
-#             if device_info["model_number"] != '0':
-#                 connections["lidar"] = ports[i]
-#                 connections["arduino"] = ports[other_idx]
-#                 end_connection = True
-#                 time.sleep(0.2)
-#                 break
-#             try_lidar.Disconnect()
-
-#         if end_connection:
-#             try_lidar.Disconnect()
-
-#         return (True, connections)
-#     else:
-#         print(f"At least {str(n_connections - n_ports)} device(s) is not connected!")
-#         return (False, connections)
-
-
-def send_data(comm_obj:SerialCommunication):
-    while True:
-        comm_obj.write(1)
-
-# def show_vision(cam_obj:PiCam, framerate):
-#     delay = round(1 / framerate, 2)
-#     while True:
-#         frame = cam_obj.capture_frame()
-#         print(f"Frame: {frame[0][0]}")
-#         time.sleep(delay)
-
-# def get_manual_command(web_obj:WebInterface):
-#     global manual_command
-#     while True:
-#         cmd = int(web_obj.get_latest_message())
-#         manual_command = web_to_serial_command[cmd]
-#         print(cmd, end=" | ")
-#         print(manual_command)
-#         return manual_command
-
 if __name__ == "__main__":
-
-    web_url = "ws://192.168.29.219:8000"
-    arduino_port, lidar_port = check_port_connections()
+    arduino_port, lidar_port = setup_serial_port()
     print(arduino_port, lidar_port)
-
-    # while True:
-    #     print("2: Forward, 3: Reverse, 4: Right, 5: Left")
-    #     order = int(input("Enter direction: "))
-    #     drive_comm.write(order)
-    #     time.sleep(2)
-
-    # if connection_status:
-    #     print(connection_check)
-        
-    #     drive_comm = SerialCommunication(
-    #         port=arduino_port, 
-    #         baud_rate=9600)
-        
-    #     camera = PiCam()
-        
-    #     interface = WebInterface(web_url)
-    #     interface.start()
-
-    #     lidar = PyLidar3.YdLidarX4(port=lidar_port)
-    #     lidar.Connect()
-
-    #     lidar_thread = threading.Thread(target=lidar_data, args=(lidar,))
-    #     sercomm_process = multiprocessing.Process(target=send_data, args=(drive_comm,))
-    #     vision_thread = threading.Thread(target=show_vision, args=(camera, 10, ))
-    #     webcomm_thread = threading.Thread(target=get_manual_command, args=(interface, ))
-        
-    #     try:
-    #         lidar_thread.start()
-    #         sercomm_process.start()
-    #         vision_thread.start()
-    #         webcomm_thread.start()
-
-    #         lidar_thread.join()
-    #         sercomm_process.join()
-    #         vision_thread.join()
-    #         webcomm_thread.start()
-
-    #     except KeyboardInterrupt:
-    #         for i in range(4):
-    #             drive_comm.write(1)
-    #             time.sleep(0.1)
-
-    #         lidar_thread.join()
-    #         sercomm_process.join()
-    #         vision_thread.join()
-
-    #         lidar.Disconnect()
-    #         drive_comm.close_connection()
-
-    # else: 
-    #     print("Bad port connection!")
-    #     exit()
