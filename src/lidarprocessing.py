9e678c963a83bf5c041f10a2066c0ed2fb8d4050
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2024-01-18 08:48:23 +0700

    Update lidarprocessing.py

--- src/lidarprocessing.py
+++ src/lidarprocessing.py
@@ -32,7 +32,7 @@ class LIDAR:
             prev_time = time.time()
             for scan in self.lidar.iter_scans():
                 c_time = time.time()
-                print(f"INTV: {c_time - prev_time} - N: {len(scan)} - SCAN: {scan[:3]}")
+                print(f"dT: {c_time - prev_time} - N: {len(scan)} - SCAN: {scan[:3]}")
                 print()
                 prev_time = c_time
         except:
